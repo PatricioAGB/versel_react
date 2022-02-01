df84b1c9f628e44a4d0db8a5a1382de833f354be
Razer81368 <Razer81368@gmail.com>
2022-02-01 17:41:43 -0300

    Parte de bastian y pato lista

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
-<<<<<<< HEAD
-import FormsPeople from "./components/FormsPeople";
-import Hola from "./components/Hola";
-import EvaluationForm from "./components/EvaluationForm";
-=======
 import FormsPeople from "./components/FormsPeople"; 
->>>>>>> 5657a7eef728b40ddbfc98b83eff7b7c8d9e12a1
+import EvaluationForm from "./components/EvaluationForm";
 import {
   BrowserRouter,
   Routes,
   Route,
   Link
 } from "react-router-dom";
+
 
 
 
@@ -34,12 +30,7 @@ function App() {
           <Route path="/" exact />
           {/* seleccionamo la ruta */}
           <Route path="/formulario" element={<FormsPeople />} />
-<<<<<<< HEAD
-          {/* seleccionamos la ruta */}
-          <Route path="/hola" element={<Hola />} />
           <Route path="/evaluacion" element={<EvaluationForm />} />
-=======
->>>>>>> 5657a7eef728b40ddbfc98b83eff7b7c8d9e12a1
         </Routes>
       </div>
     </BrowserRouter>
