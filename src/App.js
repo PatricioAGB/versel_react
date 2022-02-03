9388b29844e1eaed264bdb0ad19f4c165f91123b
BastianAcuna <BastianAcS@hotmail.com>
2022-02-03 12:38:49 -0300

    bastian

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
 import FormsPeople from "./components/FormsPeople";
-import Hola from "./components/Hola";
 import EvaluationForm from "./components/EvaluationForm";
-=======
-import FormsPeople from "./components/FormsPeople"; 
->>>>>>> 5657a7eef728b40ddbfc98b83eff7b7c8d9e12a1
 import {
   BrowserRouter,
   Routes,
@@ -23,10 +18,10 @@ function App() {
         <div className="btn-group">
           {/* Navbar */}
           <Link to="formulario" className="btn btn-dark">
-            formulario
+            Formulario
           </Link>
           <Link to="evaluacion" className="btn btn-dark" > 
-          formulario
+          Evaluación
           </Link>
         </div>
         {/* Rutas */}
@@ -34,12 +29,7 @@ function App() {
           <Route path="/" exact />
           {/* seleccionamo la ruta */}
           <Route path="/formulario" element={<FormsPeople />} />
-<<<<<<< HEAD
-          {/* seleccionamos la ruta */}
-          <Route path="/hola" element={<Hola />} />
           <Route path="/evaluacion" element={<EvaluationForm />} />
-=======
->>>>>>> 5657a7eef728b40ddbfc98b83eff7b7c8d9e12a1
         </Routes>
       </div>
     </BrowserRouter>
